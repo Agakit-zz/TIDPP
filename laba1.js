a552671a86efafa61204ea4dbd1d53ee09c3a1b5
[Agaki Tatiana] <[agaki.tatiana@gmail.com]>
2018-10-18 10:59:05 +0300

    changes in wording

--- laba1.js
+++ laba1.js
@@ -3,15 +3,15 @@ let b = process.argv[3]
 let c = process.argv[4]
 let x1,x2;
 let discriminant = b*b-4*a*c
-console.log(discriminant)
+console.log (discriminant)
 if (discriminant>0){
 x1 = (-b - Math.sqrt(discriminant))/2
 x2 = (-b + Math.sqrt(discriminant))/2
-console.log(`x1 = ${x1}\nx2 = ${x2}\ndiscriminant = ${discriminant}`)
-} else if(discriminant === 0) {
+console.log (`x1 = ${x1}\nx2 = ${x2}\ndiscriminant = ${discriminant}`)
+} else if (discriminant === 0) {
 x1 = (-b/2*a);
-console.log(`x1 = ${x1}\ndiscr = ${discriminant}`)
-} else {
-console.log('discriminant<0')
+console.log (`x1 = ${x1}\ndiscr = ${discriminant}`)
+} else if (discriminant < 0) {
+console.log ('discriminant<0')
 }
-console.log(discriminant)
+console.log (discriminant)
